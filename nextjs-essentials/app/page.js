fc0f4ff535b2a25d2d1f0b15ddefdb08229a2d36
Pavan S <iampavan05@gmail.com>
2024-06-20 13:41:36 +0530

    adding custom components

--- nextjs-essentials/app/page.js
+++ nextjs-essentials/app/page.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 
+// import Header from "../components/header";
+// While we could import the header as shown above,
+// Next.js provides import alias configuration, allowing us to import it as follows:
+import Header from "@/components/header";
+
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <Header />
       <h1>Welcome to this NextJS Course!</h1>
       <p>Let&apos;s get started! 🔥🔥🔥</p>
       <p>

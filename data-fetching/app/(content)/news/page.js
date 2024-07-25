10214e5560700dbc1d45a9f38289e067c8f0016d
Pavan S <iampavan05@gmail.com>
2024-07-25 22:05:45 +0530

    client-side fetching

--- data-fetching/app/(content)/news/page.js
+++ data-fetching/app/(content)/news/page.js
@@ -1,11 +1,48 @@
-import { DUMMY_NEWS } from '@/dummy-news';
-import NewsList from '@/components/news-list';
+"use client";
+
+import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (response.ok) {
+        const news = await response.json();
+        setLoading(false);
+        setNews(news);
+      } else {
+        setError("Failed to fetch news.");
+        setLoading(false);
+      }
+    }
+
+    fetchNews();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }

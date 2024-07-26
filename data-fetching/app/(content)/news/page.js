cfe42cf28441f7d734c4b4031d75256e23d6721f
Pavan S <iampavan05@gmail.com>
2024-07-26 18:00:22 +0530

    added suspense component

--- data-fetching/app/(content)/news/page.js
+++ data-fetching/app/(content)/news/page.js
@@ -1,48 +1,28 @@
 "use client";
 
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+
+async function News() {
+  // URL can be from outside too like -> https://jsonplaceholder.typicode.com/users
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch News.");
+  }
+
+  const news = await response.json();
+
+  return <NewsList news={news} />;
+}
 
 export default function NewsPage() {
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    async function fetchNews() {
-      setLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (response.ok) {
-        const news = await response.json();
-        setLoading(false);
-        setNews(news);
-      } else {
-        setError("Failed to fetch news.");
-        setLoading(false);
-      }
-    }
-
-    fetchNews();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
-
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <Suspense fallback={<p>Loading the news...</p>}>
+        <News />
+      </Suspense>
     </>
   );
 }

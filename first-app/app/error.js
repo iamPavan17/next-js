ff7390bbe60263f16e7e5c9a5668f1616de51cc9
iamPavan17 <iampavan05@gmail.com>
2024-09-03 12:08:00 +0530

    data-fetching

---
+++ first-app/app/error.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ErrorPage({ error }) {
+  return <div>{error.message}</div>;
+}
--- first-app/app/page.js
+++ first-app/app/page.js
@@ -1,9 +1,36 @@
+import { Suspense } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+async function Users() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(URL);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch users.");
+  }
+
+  const users = await response.json();
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>Let&apos;s get started! 🔥🔥🔥</p>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Users />
+      </Suspense>
     </main>
   );
 }
